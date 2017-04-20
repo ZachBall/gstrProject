95ff2ae4424d946e1e63b2807f2bd09e9ec36ce0
Zach Ball <ballz@berea.edu>
2017-04-20 11:15:22 -0400

    Added some stuff

--- static/js/calculate.js
+++ static/js/calculate.js
@@ -2,7 +2,10 @@ $("#resultModal").on('shown.bs.modal', function(){
 	var current = document.getElementById('currentSlider').noUiSlider;
 	var cutBack = document.getElementById('CutBack').noUiSlider;
 	
-	var CO2Out = ((((Number(current.get())*3.5)*0.0283495)*52)*35.75)
-	var CO2Cut = ((((Number(cutBack.get())*3.5)*0.0283495)*52)*35.75)
-	//return(CO2Out, CO2Cut);
+	var CO2Out = ((((Number(current.get())*3.5)*0.0283495)*52)*35.75).toFixed(2)
+	var CO2Cut = ((((Number(cutBack.get())*3.5)*0.0283495)*52)*35.75).toFixed(2)
+	console.log(CO2Out, CO2Cut);
+	document.getElementById('currentOutput').innerHTML = CO2Out.toString() + ' KG Per Year';
+	document.getElementById('outputCut').innerHTML = CO2Cut.toString() + ' KG Per Year';
+	document.getElementById('newOutput').innerHTML = (CO2Out - CO2Cut).toFixed(2).toString() + ' KG Per Year';
 });
