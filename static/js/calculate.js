bfb03ac64c8bb596e381087bfc406536b8673a85
Zach Ball <ballz@berea.edu>
2017-05-01 18:46:43 -0400

    Testing some more

--- static/js/calculate.js
+++ static/js/calculate.js
@@ -2,13 +2,13 @@ $("#resultModal").on('shown.bs.modal', function(){
 	var current = document.getElementById('currentSlider').noUiSlider;
 	var cutBack = document.getElementById('CutBack').noUiSlider;
 
-	var CO2Out = ((((Number(current.get())*52)*3.5)*0.0283495)*35.75).toFixed(2);
-	var CO2Cut = ((((Number(cutBack.get())*52)*3.5)*0.0283495)*35.75).toFixed(2)
+	var CO2Out = ((((Number(current.get())*52)*3.5)*0.0283495)*35.75);
+	var CO2Cut = ((((Number(cutBack.get())*52)*3.5)*0.0283495)*35.75);
 
-	document.getElementById('currentOutput').innerHTML = CO2Out.toString() + ' KG Per Year';
+	document.getElementById('currentOutput').innerHTML = CO2Out.toLocaleString('en', {maximumFractionDigits:0}) + ' KG Per Year';
 	document.getElementById('currentEquivalence').innerHTML = "That's the same as driving about " + Number((CO2Out / 0.347).toFixed(0)).toLocaleString('en') + " miles.";
-	document.getElementById('outputCut').innerHTML = CO2Cut.toString() + ' KG Per Year';
-	document.getElementById('newOutput').innerHTML = (CO2Out - CO2Cut).toFixed(2).toString() + ' KG Per Year';
-	document.getElementById('equivalence').innerHTML = "That's the same as driving about " + Number((CO2Cut / 0.347).toFixed(0)).toLocaleString('en') + " miles fewer."
+	document.getElementById('outputCut').innerHTML = CO2Cut.toLocaleString('en', {maximumFractionDigits:0}) + ' KG Per Year';
+	document.getElementById('newOutput').innerHTML = (CO2Out - CO2Cut).toLocaleString('en', {maximumFractionDigits:0}) + ' KG Per Year';
+	document.getElementById('equivalence').innerHTML = "That's the same as driving about " + Number((CO2Cut / 0.347)).toLocaleString('en', {maximumFractionDigits:0}) + " miles fewer."
 
 });
